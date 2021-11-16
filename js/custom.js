204e602b2449746115f92982ab9b9a9ef667eeec
Awal Bashar <awalbashar194@gmail.com>
2021-11-17 01:37:25 +0600

    update

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  AOS.init({
+    once: false
+  });
+
+  //popup cross
+  $("#cross-btn").click(function(){
+    $("#popup-sec").css('display','none');
+  });
+});
+
+
+// slider 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+//sticky header
+window.addEventListener("scroll", function() {
+  const header = document.getElementById("header"),
+        scroll = window.pageYOffset | document.body.scrollTop;
+
+  if (scroll > 300) { header.className = "header-sec sticky"; }
+  else if (scroll <= 300) { header.className = "header-sec"; }
+});
+
